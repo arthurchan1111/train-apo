6ecd2f5f573ae9340a2dd9f4b3d32620f248db8b
Arthur Chan <arthurchan1111@gmail.com>
2018-01-11 23:17:55 -0500

    Secondary import of files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-import { AppModule } from "./app.module";
+if (environment.production) {
+  enableProdMode();
+}
 
-
-platformNativeScriptDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
