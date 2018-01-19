6091e326ace9d14a07bc735adcc70cace76b85df
Arthur Chan <arthurchan1111@gmail.com>
2018-01-18 23:47:21 -0500

    Feat: Add error handling to service

--- src/app/bart_service/bart.service.ts
+++ src/app/bart_service/bart.service.ts
@@ -1,19 +1,19 @@
-/*import { Injectable } from  '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { Injectable } from  '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class BartService{
   constructor(private _httpClient: HttpClient){
 
   }
-  public getStations(){
-    this._httpClient.get("https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y")
-    .subscribe(
-      (data:any) =>{
-        console.log(data);
-      }
+  public getStations() :Observable<Station[]> {
+     return this._httpClient.get("https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y").
+     pipe(
+       tap(stations => this.log(`fetched stations`)),
+      catchError(this.handleError('getStations', []))
+     )
 
-    )
   }
 }
-*/
