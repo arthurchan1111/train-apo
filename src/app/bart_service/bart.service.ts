b306dbdabdbd96d953389cdff06d6ca87c38bc5a
Arthur Chan <arthurchan1111@gmail.com>
2018-01-23 23:45:25 -0500

    Feat: Add more bart api service methods

--- src/app/bart_service/bart.service.ts
+++ src/app/bart_service/bart.service.ts
@@ -4,13 +4,26 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class BartService{
+  private key: string = "MW9S-E7SL-26DU-VV8V";
+  private json: string= "y";
   constructor(private _httpClient: HttpClient){
 
   }
   public getStations() {
-     return this._httpClient.get("https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y");
+     return this._httpClient.get("https://api.bart.gov/api/stn.aspx?cmd=stns&key=" + this.key +"&json=" + this.json);
+}
 
+public getStationInfo(station: string){
+   return this._httpClient.get("https://api.bart.gov/api/stn.aspx?cmd=stninfo&key="+ this.key +"&json="+ this.json+ "&orig=" + station);
 
-  }
+}
+
+public getBuildingInfo(station: string){
+  return this._httpClient.get("http://api.bart.gov/api/stn.aspx?cmd=stnaccess&key=" + this.key + "&json="+ this.json + "&orig=" + station);
+}
+
+public getStationSchedule(station:string){
+    return this._httpClient.get("http://api.bart.gov/api/sched.aspx?cmd=stnsched&key="+ this.key + "&json=" + this.json+ "&l=1&orig="+station);
+}
 
 }
