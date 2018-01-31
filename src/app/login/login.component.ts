7ad3e62de3f362d4a7ed5a79c57e328de0b45530
Arthur Chan <arthurchan1111@gmail.com>
2018-01-30 23:55:30 -0500

    Feat: Login styles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,8 +4,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
+  styleUrls:['./login.scss']
 })
 
 export class LoginComponent{
+private access_token: any = Math.random().toString(36);
+
+
 
 }
