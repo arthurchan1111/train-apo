32ecbea4c2db51bd753b92adae7d42b4b31aef30
Arthur Chan <arthurchan1111@gmail.com>
2018-02-05 23:44:09 -0500

    Feat: Add station information to page

--- src/app/stationinfo/stationinfo.component.ts
+++ src/app/stationinfo/stationinfo.component.ts
@@ -7,28 +7,31 @@ import { BartService } from '../bart_service/bart.service';
 })
 
 export class StationInfoComponent implements OnInit{
-
-
+  private stationinfo: any[];
+  private buildingInfo: any[];
   constructor (private bartService : BartService){}
 
   ngOnInit(){
-    this.stationInfo();
-    this.buildingInfo();
-    console.log(this.stations);
+    this.stationInfo("12th");
+    this.buildingInfo("12th");
+    console.log(this.stationinfo);
+    console.log(this.buildingInfo);
+
   }
 
   stationInfo(station: string): void{
     this.bartService.getStationInfo(station).subscribe(
-      (data: any) => {console.log(data);
-
+      (data: any) => {
+       this.stationinfo = data.root.stations.station;
       }
     );
-
+  }
    buildingInfo(station: string): void{
       this.bartService.getBuildingInfo(station).subscribe(
-        (data: any) => {console.log(data);
-
+        (data: any) => {
+          this.buildingInfo= data.root.stations.station;
         }
       );
 
   }
+}
